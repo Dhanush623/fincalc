e53ae09ccbd365712ff4a55f02e4a7e81ac2cfab
Dhanush <40487848+Dhanush623@users.noreply.github.com>
2024-04-15 16:39:13 +0530

    v1.0.3 fixes

--- lib/src/screens/settings/settings.dart
+++ lib/src/screens/settings/settings.dart
@@ -121,7 +121,6 @@ class _SettingsState extends State<Settings> with WidgetsBindingObserver {
             ),
             trailing: Switch(
               value: isDarkTheme,
-              activeColor: const Color(0xFF01579B),
               onChanged: (bool value) {
                 Provider.of<ThemeManager>(context, listen: false).toggleTheme();
                 saveData(
@@ -141,7 +140,6 @@ class _SettingsState extends State<Settings> with WidgetsBindingObserver {
               style: TextStyle(fontSize: 18),
             ),
             trailing: Switch(
-              activeColor: const Color(0xFF01579B),
               value: isNotification,
               onChanged: (bool value) {
                 changeNotificationPermission();
