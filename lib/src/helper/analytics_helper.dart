470fdf584fe402f32bd2507d705b801f5897b53c
Dhanush <40487848+Dhanush623@users.noreply.github.com>
2024-04-18 21:49:12 +0530

    added analytics

--- lib/src/helper/analytics_helper.dart
+++ lib/src/helper/analytics_helper.dart
@@ -2,11 +2,14 @@ import 'package:finance/src/utils/constants.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 addAnalyticsLogger(String event, Map<String, Object?> parameters) async {
-  await FirebaseAnalytics.instance
-      .logEvent(name: event, parameters: parameters);
+  await FirebaseAnalytics.instance.logEvent(
+    name: event,
+    parameters: parameters,
+  );
 }
 
 addScreenViewTracking(String screenName, String screenClass) async {
+  addScreenViewLog(screenName, screenClass);
   await FirebaseAnalytics.instance.logEvent(
     name: Constants.firebaseScreenViewKey,
     parameters: {
@@ -14,4 +17,15 @@ addScreenViewTracking(String screenName, String screenClass) async {
       'firebase_screen_class': screenClass,
     },
   );
+}
+
+addScreenViewLog(String screenName, String screenClass) async {
+  await FirebaseAnalytics.instance.logScreenView(
+    screenName: screenName,
+    screenClass: screenClass,
+  );
+}
+
+logAppOpen() async {
+  await FirebaseAnalytics.instance.logAppOpen();
 }
